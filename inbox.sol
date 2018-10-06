1e146a3394590fefab69855578cc718dd6ad7a28
richardjohns <richard.johns193@gmail.com>
2018-10-06 23:15:00 +1300

    Update inbox.sol

--- inbox.sol
+++ inbox.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.4.17;
 contract Inbox {
     /// Defining message variable below automatically creates a public function
     /// which can be called ie getMessage is unnecessary duplication in this case.
-    /// Calling a function is free, instant, and can return data.
+    /// Calling a function is free, instant, and can return data. However storing data costs.
+   
     string public message;
     
     function Inbox(string initialMessage) public {
@@ -11,6 +12,7 @@ contract Inbox {
     }
     
     /// Sending a transaction to a function can modify contract data, creates a transaction hash, & costs money.
+    
     function setMessage(string newMessage) public {
         message = newMessage;
     }
